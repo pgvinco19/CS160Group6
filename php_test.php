bf6638eea365f2f1f6fa5aeb4da6808817d3d8fb
davidUng <ung_david@yahoo.com>
2017-04-10 00:47:33 -0700

    commit some php file 3

--- php_test.php
+++ php_test.php
@@ -12,68 +12,71 @@ Your email address is: <?php echo $_REQUEST["email"]; ?><br>
 
 <?php
 
-include("connection.php");
+    include("connection.php");
 
-// Check connection
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
-}
-
-$sql = "SELECT firstName, lastName, email, streetAddress, city, zipCode, phoneNumber FROM User";
-$response = mysqli_query($db, $sql);
-
-echo '<table>';
-
-if($response){
-
-    while($row = mysqli_fetch_array($response)){
-
-    echo '<tr><td align="left">' .
-        $row['firstName'] . '</td><td align="left">' .
-        $row['lastName'] . '</td><td align="left">' .
-        $row['email'] . '</td><td align="left">' .
-        $row['streetAddress'] . '</td><td align="left">' .
-        $row['city'] . '</td><td align="left">' .
-        $row['state'] . '</td><td align="left">' .
-        $row['zipCode'] . '</td><td align="left">' .
-        $row['phoneNumber'] . '</td><td align="left">';
-
-    echo '</tr>';
+    // Check connection
+    if ($db->connect_error)
+    {
+        die("Connection failed: " . $db->connect_error);
     }
 
-    echo '</table>';
+    $sql = "SELECT firstName, lastName, email, streetAddress, city, zipCode, phoneNumber FROM User";
+    $response = mysqli_query($db, $sql);
 
-}
-else {
+    if($response)
+    {
+        echo '<table>';
 
-    echo "Couldn't issue database query<br />";
+        while($row = mysqli_fetch_array($response))
+        {
 
-    echo mysqli_error($db);
+            echo '<tr><td align="left">' .
+                $row['firstName'] . '</td><td align="left">' .
+                $row['lastName'] . '</td><td align="left">' .
+                $row['email'] . '</td><td align="left">' .
+                $row['streetAddress'] . '</td><td align="left">' .
+                $row['city'] . '</td><td align="left">' .
+                $row['state'] . '</td><td align="left">' .
+                $row['zipCode'] . '</td><td align="left">' .
+                $row['phoneNumber'] . '</td><td align="left">';
 
-}
+            echo '</tr>';
+        }
+
+        echo '</table>';
+
+    }
+    else
+    {
+        echo "Couldn't issue database query<br />";
+        echo mysqli_error($db);
+    }
 
 
-$test_query = "SHOW TABLES FROM cs160database";
-$result = mysqli_query($db, $test_query);
+    $test_query = "SHOW TABLES FROM cs160database";
+    $result = mysqli_query($db, $test_query);
 
-$tblCnt = 0;
-while($tbl = mysqli_fetch_array($result))
-{
-    $tblCnt++;
-    echo $tbl[0]."<br />\n";
-}
-if (!$tblCnt) {
-    echo "There are no tables<br />\n";
-} else {
-    echo "There are $tblCnt tables<br />\n";
-}
+    $tblCnt = 0;
+    while($tbl = mysqli_fetch_array($result))
+    {
+        $tblCnt++;
+        echo $tbl[0]."<br />\n";
+    }
+
+    if (!$tblCnt)
+    {
+        echo "There are no tables<br />\n";
+    }
+    else
+    {
+        echo "There are $tblCnt tables<br />\n";
+    }
 
 
 
 
 // Close connection to the database
 mysqli_close($db);
-
 
 ?>
 
