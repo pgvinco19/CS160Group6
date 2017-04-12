7d994b6a03d013b02071a1b97b6555ff8dfe1c5e
nikiwiki <nikitha.adira@yahoo.com>
2017-04-11 23:18:24 -0700

    login

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php 
+session_start(); 
+include('connection.php'); 
+$error = ""; 
+
+if(isset($_GET["submit"])){
+	if(empty($_GET["username"]) || empty($_GET["password"])){
+		$error = "Both fields are required."; 
+	} else {
+		$username = $_GET['username']; 
+		$password = $_GET['password']; 
+
+		$username = stripcslashes($username); 
+		$password = stripslashes($password); 
+
+		$username = mysqli_real_escape_string($db, $username); 
+		$password = mysqli_real_escape_string($db, $password); 
+		$password = md5($password); 
+
+		$query = @"SELECT clientID FROM User WHERE username = '$username' and password = '$password'"; 
+		$result = mysqli_query($db, $query); 
+
+		$row = mysqli_fetch_array($result, MYSQLI_ASSOC); 
+			}
+			if(mysqli_num_rows($result) == 1){
+				$cID = $row['clientID'];
+				$_SESSION['clientID'] = $cID;
+			}else {
+				$error = "Incorrect username or password"; 
+			}
+		}
+?>
