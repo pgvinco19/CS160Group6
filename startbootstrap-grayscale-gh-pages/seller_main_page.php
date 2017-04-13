21f62dd8ca081b970394e0ba49a11c106daa6346
pgvinco19 <PVMacbook@PVMacbook.local>
2017-04-12 17:33:36 -0700

    deleted duplicate html, fixed displaying of ticket in seller main page
    deleted duplicate html, fixed displaying of ticket in seller main page

--- startbootstrap-grayscale-gh-pages/seller_main_page.php
+++ startbootstrap-grayscale-gh-pages/seller_main_page.php
@@ -1,3 +1,8 @@
+<?php session_start();
+
+$TPclientID = $_SESSION['clientID'];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -144,10 +149,24 @@
 
     <div class="container" style="overflow-x:auto;">
         <?php
+        include("../connection.php");
 
-                include("../sellerMainDisplayTickets.php");
+        //Getting tickets listing from certain seller
+        $sql = "SELECT eventName, eventDate, eventLocation, numberOfTickets FROM `Ticket_Products` WHERE TPclientID = " . $TPclientID;
+        $response = $db->query($sql);
 
-                ?>
+        if ($response->num_rows > 0)  {
+            echo "<table><tr><th>Event Name</th><th>Date</th><th>Location</th><th>Quantity</th></tr>";
+            // output data of each row
+            while($row = $response->fetch_assoc()) {
+                echo "<tr><td>".$row["eventName"]."</td><td>".$row["eventDate"]."</td><td>".$row["eventLocation"]."</td><td>".$row["numberOfTickets"]."</td></tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "0 results";
+        }
+        mysqli_close($db);
+        ?>
     </div>
 
     <p></p>
